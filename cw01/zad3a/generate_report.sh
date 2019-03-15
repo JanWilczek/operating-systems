c3a4bf88472295af1f3d23a4a5289d4c159705c7
Jan Wilczek <janekw@vp.pl>
2019-03-15 01:26:19 +0100

    WIP: First part of dynamically loaded library.

---
+++ cw01/zad3a/generate_report.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+OUTPUT_FILE_NAME=raport3a.txt
+
+make clean
+rm -f $OUTPUT_FILE_NAME
+
+make speed_test_static
+./speed_test_static $OUTPUT_FILE_NAME "Static library:"
+
+make speed_test_shared
+./speed_test_shared $OUTPUT_FILE_NAME "Shared library:"
+
+make speed_test_dynamic

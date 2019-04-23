58573784e02be4735cfa8a5593b4a23bf131cbeb
Jan Wilczek <janekw@vp.pl>
2019-04-23 13:47:25 +0200

    6.1: Client queue removal at exit or interrupt.

--- cw06/zad1/client.c
+++ cw06/zad1/client.c
@@ -1,22 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 #include "queue_common.h"
 #include "ipc_queue_sv.h"
 
+ipc_queue_t *queue;
+ipc_queue_t *server_queue;
 
-int main(int argc, char* argv[])
+void client_exit(void)
 {
+    if (queue)
+    {
+        remove_queue(queue);
+    }
+
+    if (server_queue)
+    {
+        free(server_queue);
+    }
+}
+
+void sigint_handler(int signum)
+{
+    if (signum == SIGINT)
+    {
+        client_exit();
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    // Initialize the global variables
+    queue = NULL;
+    server_queue = NULL;
+
+    // Handle normal process termination
+    if (atexit(client_exit) != 0)
+    {
+        fprintf(stderr, "Cannot set exit function of the client. Terminating.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Handle process interruption by a SIGINT signal
+    struct sigaction handler;
+    handler.sa_handler = sigint_handler;
+    sigemptyset(&handler.sa_mask);
+    handler.sa_flags = 0;
+    sigaction(SIGINT, &handler, NULL);
+
     // 1. Create queue with unique IPC key
-    ipc_queue_t* queue = create_queue(CLIENT_QUEUE);
+    queue = create_queue(CLIENT_QUEUE);
     printf("Client queue ID is %d\n", queue->id);
 
-    // 1.a. Handle SIGINT signal
-    ipc_queue_t* server_queue = get_queue(ftok(getenv("HOME"), SERVER_QUEUE_PROJ_ID));
+    // 2.a. Get server queue reference
+    server_queue = get_queue(ftok(getenv("HOME"), SERVER_QUEUE_PROJ_ID));
     printf("Server queue ID is %d\n", server_queue->id);
 
-    remove_queue(queue);
-
-    // 2. Send the key to the server
+    // 2.b. Send the key to the server
 
     // 3. Receive client ID
 

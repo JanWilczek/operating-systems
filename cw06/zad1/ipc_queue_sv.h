c35f15c3d3ff92b1bc6ff1ce75cc8f77381e3785
Jan Wilczek <janekw@vp.pl>
2019-05-10 00:05:45 +0200

    6.1: Documentation notes.

--- cw06/zad1/ipc_queue_sv.h
+++ cw06/zad1/ipc_queue_sv.h
@@ -15,11 +15,17 @@ typedef struct ipc_queue {
 
 /**
  * Creates an IPC queue and returns its ID.
+ * 
+ * @param type of the queue-for server returns a queue with an established id
+ * and for client generates a random key.
+ * @return a pointer to allocated ipc_queue_t structure.
  * */
 extern ipc_queue_t* create_queue(enum QueueType type);
 
 /**
  * Returns existing IPC queue.
+ * @param key to the queue
+ * @return a pointer to allocated ipc_queue_t structure.
  * */
 extern ipc_queue_t* get_queue(key_t key);
 
@@ -29,11 +35,26 @@ extern ipc_queue_t* get_queue(key_t key);
 extern void remove_queue(ipc_queue_t* queue_to_remove);
 
 /**
- * Returns 0 on success and -1 on failure. 
- * When -1 is returned and errno is set to ENOMSG (when block is 1) then there is no message of the given type in the queue.
+ * Receives a message sent with send_message
+ * @param queue to receive from
+ * @param buffer to store the message into
+ * @param size of the buffer to store the message into
+ * @param type of the message to receive (0 for all messages)
+ * @param 1 if call should be blocking, 0 if it should return -1 
+ * if no message of given type is in the queue (see below).
+ * @return 0 on success and -1 on failure. 
+ * When -1 is returned and errno is set to ENOMSG (when block is 1) then there is 
+ * no message of the given type in the queue.
  * */
 extern int receive_message(ipc_queue_t* queue, char* buffer, size_t buffer_size, long* type, int block);
 
+/**
+ * Sends a message to an IPC queue, that can be received via receive_message.
+ * @param pointer to the queue to send the message to
+ * @param message string
+ * @param type of the message
+ * @return 0 on success, -1 on failure
+ * */
 extern int send_message(ipc_queue_t* queue, const char* buffer, long type);
 
 /**

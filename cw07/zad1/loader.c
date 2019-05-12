52bc3af645500c01a165745e8a3fae8b28fbe669
Jan Wilczek <janekw@vp.pl>
2019-05-12 16:56:33 +0200

    7: WIP: Argument parsing.

--- cw07/zad1/loader.c
+++ cw07/zad1/loader.c
@@ -1,10 +1,19 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "semaphore.h"
 #include "tape.h"
 
 semaphore_t* truck_ready;
 semaphore_t* tape_count;
 // semaphore_t* tape_load;
+
+void print_usage(const char* program_name)
+{
+    fprintf(stderr, "Usage:     %s N [C]\n"
+                    "   N       weight of the packages from this laoder\n"
+                    "   C       number of packages to give (0 or lack of"
+                    "           argument means an infinite number of packages\n", program_name);
+}
 
 void loader_loop(int N)
 {
@@ -16,6 +25,8 @@ void loader_loop(int N)
 
 void loader(int N, int C)
 {
+    // TODO: get all needed semaphores
+
     if (C == 0)
     {
         while (1)
@@ -34,5 +45,23 @@ void loader(int N, int C)
 
 int main(int argc, char* argv[])
 {
+    int C = 0;
+    int N;
+
+    if (argc > 2 || argc == 0)
+    {
+        print_usage(argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    N = atoi(argv[0]);
+
+    if (argc == 2)
+    {
+        C = atoi(argv[1]);
+    }
+    
+    loader(N, C);
+
     return EXIT_SUCCESS;
 }
--- cw07/zad1/trucker.c
+++ cw07/zad1/trucker.c
@@ -4,19 +4,19 @@
 #include "semaphore.h"
 #include "tape.h"
 
-semaphore_t* tape_count;
-semaphore_t* is_package;
+semaphore_t *tape_count;
+semaphore_t *is_package;
 // semaphore_t* tape_load;
-semaphore_t* truck_ready;
+semaphore_t *truck_ready;
 
-void trucker(int X)
+void trucker_loop(int X)
 {
     int count = 0;
-    while(1)
+    while (1)
     {
         sem_wait_one(is_package);
-        int package_mass = tape_get_package(); 
-        count++;    // if X means mass then it should be `count += package_mass`. We assume that X stands for package count.
+        int package_mass = tape_get_package();
+        count++; // if X means mass then it should be `count += package_mass`. We assume that X stands for package count.
 
         if (count > X)
         {
@@ -34,11 +34,23 @@ void trucker(int X)
         }
 
         // Signal that truck is ready for loading
-        sem_signal_one(truck_ready); 
+        sem_signal_one(truck_ready);
     }
 }
 
-int main(int argc, char* argv[])
+void trucker(int X)
 {
+    // TODO: get all needed semaphores
+    
+    trucker_loop(X);
+}
+
+int main(int argc, char *argv[])
+{
+    // argumentx X, K and M.
+    int X, K, M;
+
+    trucker(X);
+    
     return EXIT_SUCCESS;
 }

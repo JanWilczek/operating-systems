48a99d3820bd19d267519ae49065699eaadb030d
Jan Wilczek <janekw@vp.pl>
2019-05-14 17:42:30 +0200

    7.1: SIGINT handler and semaphore creation.

--- cw07/zad1/trucker.c
+++ cw07/zad1/trucker.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 #include "semaphore.h"
 #include "tape.h"
 
@@ -8,6 +9,30 @@ semaphore_t *tape_count;
 semaphore_t *is_package;
 // semaphore_t* tape_load;
 semaphore_t *truck_ready;
+
+void sigint_handler(int signum)
+{
+    if (signum == SIGINT)
+    {
+        if (tape_count)
+        {
+            sem_remove(tape_count);
+            tape_count = NULL;
+        }
+
+        if (is_package)
+        {
+            sem_remove(is_package);
+            is_package = NULL;
+        }
+
+        if (truck_ready)
+        {
+            sem_remove(truck_ready);
+            truck_ready = NULL;
+        }
+    }
+}
 
 void trucker_loop(int X)
 {
@@ -38,21 +63,36 @@ void trucker_loop(int X)
     }
 }
 
-void trucker(int X)
+void trucker(int X, int K, int M)
 {
-    // TODO: get all needed semaphores
-    
+    truck_ready = sem_init("~/truck_read", X);
+    tape_count = sem_init("~/tape_count", K);
+    is_package = sem_init("~/is_package", 0);
+
     trucker_loop(X);
 }
 
 int main(int argc, char *argv[])
 {
+    tape_count = NULL;
+    is_package = NULL;
+    truck_ready = NULL;
+
+    // Register SIGINT handler
+    struct sigaction sa;
+    sa.handler = sigint_handler;
+    sigemptyset(&sa.mask);
+    sa.flags = 0;
+    if (sigaction(SIGINT, &sa, NULL) != 0)
+    {
+        perror("sigaction");
+        exit(EXIT_FAILURE);
+    }
+
     // argumentx X, K and M.
     int X, K, M;
 
-    trucker(X);
-    // semaphore_t* semaphore = sem_init("~/Documents/School/OperatingSystems/cw07/zad1/trucker_queue", 1);
-    // sem_remove(semaphore);
+    trucker(X, K, M);
 
     return EXIT_SUCCESS;
 }

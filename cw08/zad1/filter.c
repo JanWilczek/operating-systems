86bcbab3230fcf98bffe50fd2214bff2ccae6236
Jan Wilczek <janekw@vp.pl>
2019-05-28 20:25:26 +0200

    8.1: Working file parsing.

--- cw08/zad1/filter.c
+++ cw08/zad1/filter.c
@@ -6,8 +6,16 @@
 void filter_image(const char* input_image_path, const char* filter_path, const char* output_image_path, int num_threads, int is_block)
 {
     int width, height;
-    if (pgm_read(input_image_path, &width, &height) == NULL)
+    int** image;
+    if ((image = pgm_read(input_image_path, &width, &height)) == NULL)
     {
         fprintf(stderr, "Error when parsing file.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pgm_write(output_image_path, image, width, height) == 1)
+    {
+        fprintf(stderr, "Error when writing file.\n");
+        exit(EXIT_FAILURE);
     }
 }
--- cw08/zad1/pgm_io.c
+++ cw08/zad1/pgm_io.c
@@ -10,24 +10,27 @@ int** pgm_read(const char* filepath, int* width, int* height)
     FILE* file = fopen(filepath, "r");
     if (file != NULL)
     {   
-        int width, height;
-        if (fscanf(file, "P2\n%d %d\n", &width, &height) != 2)
+        int M;
+        if (fscanf(file, "P2\n%d %d\n%d\n", width, height, &M) != 3)
         {
             perror("fscanf");
             return image;
         }
+        M = M + 2;  // for compiler warning removal
 
-        image = malloc(height * sizeof(int*));
-        for (int i = 0; i < height; ++i)
+        image = malloc(*height * sizeof(int*));
+        for (int i = 0; i < *height; ++i)
         {
-            image[i] = malloc(width * sizeof(int));
+            image[i] = malloc(*width * sizeof(int));
         }
 
-        for (int row = 0; row < height; ++row)
+        for (int row = 0; row < *height; ++row)
         {
-            for (int col = 0; col < width; ++col)
+            for (int col = 0; col < *width; ++col)
             {
-                int value = fgetc(file);
+                // int value = fgetc(file);
+                int value;
+                fscanf(file, "%d", &value);
                 if (value == EOF)
                 {
                     fprintf(stderr, "Invalid input image format.\n");
@@ -49,5 +52,30 @@ int** pgm_read(const char* filepath, int* width, int* height)
 
 int pgm_write(const char* filepath, int** image, int width, int height)
 {
+    FILE* file = fopen(filepath, "w");
+    if (file != NULL)
+    {
+        fprintf(file, "P2\n");
+        fprintf(file, "%d %d\n255\n", width, height);
+        for (int row = 0; row < height; ++row)
+        {
+            for (int col = 0; col < width; ++col)
+            {
+                fprintf(file, "%d ", image[row][col]);
+            }
+            fprintf(file, "\n");
+        }
+
+        if (fclose(file) == EOF)
+        {
+            perror("fclose");
+            return -1;
+        }
+    }
+    else 
+    {
+        return -1;
+    }
+
     return 0;
 }

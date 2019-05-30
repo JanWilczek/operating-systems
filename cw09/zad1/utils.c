123cf25c4848dee5ed06564ef645b93e2f22f271
Jan Wilczek <janekw@vp.pl>
2019-05-30 18:38:39 +0200

    9.1: Added reworked functions from set 7.

---
+++ cw09/zad1/utils.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "utils.h"
+
+
+char* get_precise_time(void)
+{
+    struct timespec spec;
+    clock_gettime(CLOCK_REALTIME, &spec);
+    return format_time(&spec);
+}
+
+char* format_time(struct timespec* spec)
+{
+    long ms = (long)(spec->tv_nsec / 1e6f) % 1000L;
+
+    char* buffer = malloc(100 * sizeof(char));
+    char temp[50];
+    time_t seconds = (time_t) spec->tv_sec;
+    struct tm* tms = localtime(&seconds);
+    strftime(temp, sizeof(temp), "%H:%M:%S", tms);
+    sprintf(buffer, "%s:%ld", temp, ms);
+
+    return buffer;
+}
+
+void print_message(const char *message, const char* preamble)
+{
+    char *time_stamp = get_precise_time();
+    printf("%s %s %s\n", time_stamp, preamble, message);
+    free(time_stamp);
+}

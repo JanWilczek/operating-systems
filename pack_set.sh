0c769abafe3106d351f4b0347898d9964396a694
Jan Wilczek <janekw@vp.pl>
2019-06-01 22:17:34 +0200

    pack_set improved: no need to copy the script, just pass the name of the folder with set (e.g. cw09).

--- pack_set.sh
+++ pack_set.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Check if an argument has been passed and if so, if it is a directory (should be)
+if [ $# -eq 1 ] && [ -d $1 ]; then
+    cd $1
+fi
+
 SET_NAME=$(basename `exec pwd`)
 
 mkdir -p "WilczekJan/$SET_NAME"

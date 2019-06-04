b11cfc836e95ed8e4b1004736e73e5d8ff875e7c
Jan Wilczek <janekw@vp.pl>
2019-06-04 12:52:10 +0200

    10.1: Server arguments parsing.

---
+++ cw10/zad1/server.c
@@ -0,0 +1,6 @@
+#include "server.h"
+
+void run_server(int port_number, const char* socket_path)
+{
+    
+}
---
+++ cw10/zad1/server.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void run_server(int port_number, const char* socket_path);

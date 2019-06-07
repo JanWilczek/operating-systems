4421dfc434bef5dfe178fc5aa3d328735bf47eb1
Jan Wilczek <janekw@vp.pl>
2019-06-07 11:00:19 +0200

    10.1: A little more fine-grained server start-up.

--- cw10/zad1/stream_networking.c
+++ cw10/zad1/stream_networking.c
@@ -8,17 +8,15 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-void server_open_connection(int port_number, const char* socket_path)
-{
-    // Necessary for early process termination
-    unlink(socket_path);
 
+int start_up(const char* socket_path)
+{
     // Create local socket
     int socket_descriptor;
     if ((socket_descriptor = socket(AF_UNIX, SOCK_STREAM, 0)) == -1)
     {
         perror("socket");
-        return;
+        return -1;
     }
 
     // Bind socket to its name
@@ -29,14 +27,39 @@ void server_open_connection(int port_number, const char* socket_path)
     if (bind(socket_descriptor, (struct sockaddr *) &server_address, sizeof(struct sockaddr_un) ) == -1)
     {
         perror("bind");
-        return;
+        return -1;
     }
 
     // Listen for client connections
     if (listen(socket_descriptor, MAX_CONNECTIONS) == -1)
     {
         perror("listen");
+        return -1;
     }
+
+    return socket_descriptor;
+}
+
+void shut_down(int socket_descriptor)
+{
+    if (shutdown(socket_descriptor, SHUT_RDWR) == -1)
+    {
+        perror("shutdown");
+    }
+
+    if (close(socket_descriptor) == -1)
+    {
+        perror("close");
+    }
+
+}
+
+void server_open_connection(int port_number, const char* socket_path)
+{
+    // Necessary for early process termination
+    unlink(socket_path);
+
+    int socket_descriptor = start_up(socket_path);
 
     // Accept incoming connections
     int cliend_sockfd;
@@ -58,15 +81,7 @@ void server_open_connection(int port_number, const char* socket_path)
         printf("Received from client %s: %s", client_address.sa_data, buffer);
     }
 
-    if (shutdown(socket_descriptor, SHUT_RDWR) == -1)
-    {
-        perror("shutdown");
-    }
-
-    if (close(socket_descriptor) == -1)
-    {
-        perror("close");
-    }
+    shut_down(socket_descriptor);
 
     // Remove created socket
     unlink(socket_path);
